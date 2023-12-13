f293239ae706238e45424dfe1ef6c6184caeb51d
Saish Borkar <saish@thelegalcapsule.com>
2023-12-13 21:02:55 +0530

    feat:savepoint;

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => res.type('html').send(html));
+
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Saish!</title>
+  </head>
+  <body>
+    <section>
+      Hello from Saish!
+    </section>
+  </body>
+</html>
+`
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
